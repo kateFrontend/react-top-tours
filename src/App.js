0cb3761727f42b171febc08a09a7b39994d57f55
kateFrontend <ecdanilova@gmail.com>
2022-11-18 09:15:36 -0500

    setup loading

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ import './App.css'
 import { data } from './data'
 
 function App() {
+
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState(data);
+  if(loading){
+    return (
+      <main>
+        <Loading/>
+      </main>
+    )
+  }
+
   return <h2>Tours Project Setup</h2>
 }
 
