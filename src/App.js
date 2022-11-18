e7b71a3165d5e320c4c10d911cf83960cd8cca77
kateFrontend <ecdanilova@gmail.com>
2022-11-18 09:10:10 -0500

    add data

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
 import './App.css'
-
-const url = 'https://course-api.com/react-tours-project'
+import { data } from './data'
 
 function App() {
   return <h2>Tours Project Setup</h2>
---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const data = [
+    {
+        "id": 1,
+        "name": "Caribbean Island Tour",
+        "info": "Start in Nassau and end in Bridgetown! With the Explorer tour Caribbean Sojourn, you have a 13 days tour package taking you through Nassau, Bahamas and 6 other destinations in Latin America. Caribbean Sojourn includes accommodation, an expert guide, meals, transport and more.",
+        "image": "https://images.unsplash.com/photo-1517154868524-c6b5fbd62a1c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+        "price": "2,395"
+        },
+        {
+        "id": 2,
+        "name": "Dominican Republic Tour",
+        "info": "Start and end in Punta Cana! With the In-depth Cultural tour Dominican Republic: See & Experience Almost it ALL in 6 Days, 1st Class Traveling, you have a 6 days tour package taking you through Punta Cana, Dominican Republic and 4 other destinations in Dominican Republic. Dominican Republic: See & Experience Almost it ALL in 6 Days, 1st Class Traveling includes accommodation in a hotel as well as an expert guide, meals, transport and more.",
+        "image": "https://images.unsplash.com/flagged/photo-1574342961277-590aa4596552?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+        "price": "2,895"
+        },
+        {
+        "id": 3,
+        "name": "Mexico Tour",
+        "info": "Start and end in Cancun! With the In-depth Cultural tour Wonders of the Maya, you have a 8 days tour package taking you through Cancun, Mexico and 10 other destinations in Mexico. Wonders of the Maya includes accommodation in a hotel as well as an expert guide, meals, transport and more.",
+        "image": "https://images.unsplash.com/photo-1616423841125-8307665a0469?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+        "price": "2,695"
+        },
+        {
+        "id": 4,
+        "name": "Thailand Tour",
+        "info": "Start in Bangkok and end in Ko Pha Ngan! With the Active Adventure tour Thai Intro 9 Day, you have a 9 days tour package taking you through Bangkok, Thailand and 2 other destinations in Thailand. Thai Intro 9 Day includes accommodation, an expert guide, meals, transport and more.",
+        "image": "https://images.unsplash.com/photo-1506665531195-3566af2b4dfa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+        "price": "2,095"
+        },
+        {
+        "id": 5,
+        "name": "Philippines Tour",
+        "info": "Start and end in Puerto Princesa! With the In-depth Cultural tour Puerto Princesa & El Nido Adventure 8D/7N, you have a 8 days tour package taking you through Puerto Princesa, Philippines and 2 other destinations in Philippines. Puerto Princesa & El Nido Adventure 8D/7N includes accommodation, an expert guide, meals, transport and more.",
+        "image": "https://images.unsplash.com/photo-1518509562904-e7ef99cdcc86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+        "price": "2,595"
+        },
+        {
+            "id": 6,
+            "name": "Miami Tour",
+            "info": "Start in Miami and end in Colon! With the Explorer tour Caribbean Vibes & Ocean Exploration (7 destinations), you have a 10 days tour package taking you through Miami, USA and 6 other destinations. Caribbean Vibes & Ocean Exploration (7 destinations) includes accommodation, an expert guide, meals, transport and more.",
+            "image": "https://images.unsplash.com/photo-1514214246283-d427a95c5d2f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1080&q=80",
+            "price": "2,550"
+            }
+]
